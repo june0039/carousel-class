a8f1844d8fdb2a1e7e88149e4befa6a0eeb81215
june0039 <74568248+june0039@users.noreply.github.com>
2022-03-22 14:59:12 -0400

    btn

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const $images = document.querySelector('.carousel_images').children
+const $leftBtn = document.querySelector('.carousel_left')
+const $rightBtn = document.querySelector('.carpusel_right')
 
+let active = 0;
+
+for (const $image of $images) {
+    $image.style.display = 'none'
+
+}
+
+$images[active].style.display = 'block'
+
+function forward() {
+    $images[active].style.display = 'none'
+    active++
+
+    if (active >= $images.length) active = 0
+    
+    $images[active].style.display = 'block'
+    console.log(active)
+}
+
+$rightBtn.addEventListener('click', function () {
+    forward()
+})
