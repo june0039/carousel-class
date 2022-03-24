de62db1cec407735dc724810b18d96a717c1cb43
june0039 <74568248+june0039@users.noreply.github.com>
2022-03-24 14:14:25 -0400

    pips

--- script.js
+++ script.js
@@ -4,16 +4,40 @@ const $rightBtn = document.querySelector('.carpusel_right')
 
 const $pipsWrapper = document.querySelector('carousel_pips')
 
+
 let active = 0
 
-for (const $image of $images) {
+for (let i = 0; i < $images.length; i++) {
+
     let $pip = document.createElement('button')
     $pipsWrapper.appendChild($pip)
-    $pip.classList.add
+    $pip.classList.add('carousel_pip')
+    $pip.setAttribute('data-index', i)
 
 }
 
+const $pips = document.querySelector.('.carousel_pips').children
+
+
 $images[active].classList.add('carousel--show')
+
+$pipsWrapper.addEventListener('click', function (e)
+{
+    if (e.target.classList.contains('carousel_pip')) {
+        $images[active].classList.remove('carousel--show')
+        
+        active = e.target.dataset.index
+
+        $images[active].classList.add('carousel--show')
+        $pips[active].classList.add('carousel--active')
+
+
+        clearInterval(timer)
+        timer = setInterval(forward, 400)
+        
+    }
+    
+})
 
 function backward() {
 
@@ -23,6 +47,8 @@ function backward() {
     if ([active] == undefined) active = $images.length - 1
     $images[active].classList.add('carousel--show')
     
+     clearInterval(timer)
+     timer = setInterval(forward, 400)
     
 }
 
@@ -39,6 +65,9 @@ function forward() {
     if (active >= $images.length) active = 0
     
     $images[active].classList.add('carousel--show')
+
+     clearInterval(timer)
+     timer = setInterval(forward, 400)
     
 }
 
